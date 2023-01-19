ef88ff96a0d86aa4dcf51c9fa0515dcb892f2d9e
Alex-Prokhorenko <galexthunder@gmail.com>
2023-01-19 12:17:35 +0700

    feat: add seeder categoriesseeder for table categories

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            ['category' => 'electronics'],
+            ['category' => 'jewelery'],
+            ['category' => "men's clothing"],
+            ['category' => "women's clothing"],
+        ]);
+    }
+}
