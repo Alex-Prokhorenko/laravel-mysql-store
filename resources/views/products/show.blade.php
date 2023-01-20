41c59de5c71d67f3385869a233758e49b8068c27
Alex-Prokhorenko <galexthunder@gmail.com>
2023-01-20 12:44:50 +0700

    feat: use an asset to return links to images

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,9 +1,8 @@
-@dump($products)
 @foreach($products as $product)
     <div>
         <p>{{$product->title}}</p>
         <p>{{$product->price}}</p>
-        <img src="../{{$product->image}}" alt="{{$product->image}}" width="300"/>
+        <img src="{{asset('../'.$product->image)}}" alt="{{asset($product->image)}}" width="300"/>
         <p>{{$product->description}}</p>
-    </div>
+     </div>
 @endforeach
