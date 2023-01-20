60f7f64e94294be28b185d9aff670dd78e435a1b
Alex-Prokhorenko <galexthunder@gmail.com>
2023-01-20 10:20:44 +0700

    feat: add vue for products

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,9 @@
+
+@foreach($products as $product)
+    <div>
+        <p>{{$product->title}}</p>
+        <p>{{$product->price}}</p>
+        <img src="../{{$product->image}}" alt="{{$product->image}}" width="300"/>
+        <p>{{$product->description}}</p>
+    </div>
+@endforeach
