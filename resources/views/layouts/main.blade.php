ecbdc458df4753cc2441b57e50399105cc291b94
Alex-Prokhorenko <galexthunder@gmail.com>
2023-01-27 11:07:31 +0700

    fix: improve css style

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>fake laravel shop</title>
 </head>
 
-<body class="em">
-@yield('content')
+<body>
+    <main>
+        @yield('content')
+    </main>
 </body>
 </html>
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.main')
 @section('content')
-@foreach($products as $product)
-    <div class="product">
-        <p>{{$product->title}}</p>
-        <p>{{$product->price}}</p>
-        <img src="{{asset('../'.$product->image)}}" alt="{{asset($product->image)}}" width="300"/>
-        <p>{{$product->description}}</p>
-     </div>
-@endforeach
+    @foreach($products as $product)
+        <div class="product">
+            <p>{{$product->title}}</p>
+
+            <div class="img_container">
+                <img src="{{asset('../'.$product->image)}}" alt="{{asset($product->image)}}" width="300"/>
+            </div>
+            <p>{{$product->price}} $</p>
+            <div>
+                <p>{{$product->description}}</p>
+            </div>
+        </div>
+    @endforeach
 @endsection
