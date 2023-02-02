3ff56a02e9281deaa24b303d31ca03382722102b
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-02 11:49:27 +0700

    feat: change route structure. add route admin/product

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\MainController;
 use App\Http\Controllers\Product\CreateController;
 use App\Http\Controllers\Product\DeleteController;
 use App\Http\Controllers\Product\EditController;
-
-use App\Http\Controllers\Product\IndexController;
 use App\Http\Controllers\Product\ShowController;
 use App\Http\Controllers\Product\StoreController;
 use App\Http\Controllers\Product\UpdateController;
@@ -26,16 +24,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/products', IndexController::class)->name('products.index');
-Route::get('/products/create', CreateController::class)->name('products.create');
-Route::post('/products', StoreController::class)->name('products.store');
-Route::get('/products/{product}', ShowController::class)->where('id', '[0-9]+')
-    ->name('products.show');
-Route::get('/products/{product}/edit', EditController::class)->name('products.edit');
-Route::patch('/products/{product}', UpdateController::class)->name('products.update');
-Route::delete('/products/{product}', DeleteController::class)->name('products.delete');
+Route::group(['namespace' => 'App\Http\Controllers\Product'], function() {
+    Route::get('/products', 'IndexController')->name('products.index');
+    Route::get('/products/create', 'CreateController')->name('products.create');
+    Route::post('/products', 'StoreController')->name('products.store');
+    Route::get('/products/{product}', 'ShowController')->where('id', '[0-9]+')
+        ->name('products.show');
+    Route::get('/products/{product}/edit', 'EditController')->name('products.edit');
+    Route::patch('/products/{product}', 'UpdateController')->name('products.update');
+    Route::delete('/products/{product}', 'DeleteController')->name('products.delete');
+});
+
 
 Route::get('/main', MainController::class)->name('main.index');
+
+
+Route::prefix('admin')->namespace('App\Http\Controllers\Admin\product')->name('admin.')->group(function() {
+
+    Route::get('/product', 'IndexController')->name('product.index');
+
+});
+
 
 
 
