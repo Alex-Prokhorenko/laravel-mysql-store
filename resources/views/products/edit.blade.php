f1b4d3cdb11e27ece690984f1e7bc80e92dc17f8
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-04 15:10:22 +0700

    fix: use bootstrap styles for product change form

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,40 +1,37 @@
 @extends('layouts.main')
 @section('content')
-    <div class="create">
+    <div class="container">
         <form action="{{ route('products.update', $product) }}" method="POST">
             @csrf
             @method('patch')
-            <ul>
-                <li class="form-row">
-                    <label for="title">Enter title: </label>
-                    <input id="title" name="title" type="text" value="{{ $product->title }}"/>
-                </li>
-                <li class="form-row">
-                    <label for="price">Enter price:</label>
-                    <input id="price" name="price" type="text" value="{{ $product->price }}"/>
-                </li>
-                <li class="form-row">
-                    <label for="category_id">Select a category:</label>
-                    <select id="category_id" name="category_id">
-                        @foreach($categories as $category)
-                            <option
-                                {{ $category->id === $product->category->id ? ' selected' : ''}}
-                                value="{{ $category->id }}">{{ $category->category }}</option>
-                        @endforeach
-                    </select>
-                </li>
-                <li class="form-row">
-                    <label for="description">Enter description:</label>
-                    <input id="description" name="description" type="text" value="{{ $product->description }}"/>
-                </li>
-                <li class="form-row">
-                    <label for="image">Enter image:</label>
-                    <input id="image" name="image" type="text" value="{{ $product->image }}"/>
-                </li>
-                <li class="form-row">
-                    <input type="submit" value="update"/>
-                </li>
-            </ul>
+            <div class="mb-3">
+                <label for="title" class="form-label">Enter title: </label>
+                <input id="title" name="title" type="text" value="{{ $product->title }} " class="form-control"/>
+            </div>
+            <div class="mb-3">
+                <label for="price">Enter price:</label>
+                <input id="price" name="price" type="text" value="{{ $product->price }}" class="form-control"/>
+            </div>
+            <div class="mb-3">
+                <label for="category_id" class="form-label">Select a category:</label>
+                <select id="category_id" name="category_id" class="form-select">
+                    @foreach($categories as $category)
+                        <option
+                            {{ $category->id === $product->category->id ? ' selected' : ''}}
+                            value="{{ $category->id }}">{{ $category->category }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Enter description:</label>
+                <input id="description" name="description" type="text" value="{{ $product->description }}" class="form-control"/>
+            </div>
+            <div class="mb-3">
+                <label for="image" class="form-label">Enter image:</label>
+                <input id="image" name="image" type="text" value="{{ $product->image }}" class="form-control"/>
+            </div>
+
+            <input type="submit" value="update" class="btn btn-primary"/>
         </form>
     </div>
 @endsection
