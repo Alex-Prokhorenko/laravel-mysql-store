4992cb714ac9755b44d19c3a5f1b298a7d59dbc7
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-05 09:38:18 +0700

    feat: add policy to hide links if you are not an administrator

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,8 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Laravel Fake Store API</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -28,22 +29,28 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('products.index') }}">Products</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">Sign in</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">Sign up</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Sign in</a>
+                    </li>
+                @can('view', auth()->user())
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Sign up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('admin.product.index') }}">Admin Panel</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                @endcan
             </ul>
         </div>
     </div>
 </nav>
-    <main>
-        @yield('content')
-    </main>
+<main>
+    @yield('content')
+</main>
 </body>
 </html>
 
