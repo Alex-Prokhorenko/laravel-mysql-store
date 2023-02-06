0b5696aa0b46ee862f1f86c9b2ed874762dbbb85
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-06 12:54:10 +0700

    feat: change controller and model. Now They take pagination options

--- app/Http/Controllers/Product/IndexController.php
+++ app/Http/Controllers/Product/IndexController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Http\Filters\ProductFilter;
+use App\Http\Requests\Product\FilterRequest;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(FilterRequest $request)
     {
-        //$products = Product::paginate(10);
-        $products = Product::all();
+        $data = $request->validated();
+        $page = $data['page'] ?? 1;
+        $perPage = $data['per_page'] ?? 10;
+        $filter = app()->make(ProductFilter::class, ['queryParams' => array_filter($data)]);
+        $products = Product::filter($filter)->paginate($perPage, ['*'], 'page', $page);
+        //$products = Product::all();
 
         return ProductResource::collection($products);
         //return view('products.index', compact('products'));
