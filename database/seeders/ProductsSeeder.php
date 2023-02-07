2439177505cff8a9d9b0369d774534cbae2679b3
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-07 13:38:28 +0700

    fix: add products to migration, add images

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -17,32 +17,66 @@ class ProductsSeeder extends Seeder
     public function run()
     {
         DB::table('products')->insert([
-        [
-            "title" => "Mens Heavy Fleece Lined Sherpa Hoodie Plaid Flannel Jacket Hood Coat Sweatshirt",
-            "price" => "36.71",
-            "category_id" => "3",
-            "description" => "great jacket for every day",
-            "image" => "public\images\products\jacket1.jpg"],
-        [
-            "title" => "Men's Ripped Skinny Jeans Stretch Trousers Casual Slim Daily Chic Denim Pants",
-            "price" => "26.99",
-            "category_id" => "3",
-            "description" => "stylish black joggers jeans",
-            "image" => "public\images\products\jeans1.jpg"],
-        [
-            "title" => "Women's Summer Dress Casual Belt Vintage Retro 50s Short Sleeve Dotted",
-            "price" => "19.90",
-            "category_id" => "4",
-            "description" => "cute women black white dotted dress",
-            "image" => "public\images\products\dress1.jpg"
-        ],
-        [
-            "title" => "Men's Retro Stainless Steel Vintage Crystal Rhinestone Army Military Rings",
-            "price" => "3.29",
-            "category_id" => "2",
-            "description" => "cool men silver ring",
-            "image" => 'public\images\products\ring1.jpg'
-        ],
-    ]);
+
+            [
+                "title" => "OEM PlayStation 2 PS2 Clear Ocean Blue Controller DualShock 2 SCPH-10010",
+                "price" => "29",
+                "category_id" => "1",
+                "description" => "Includes all the functions of digital joy-pad.
+                12 Analog keys in 3D mode add variety and intelligence in operation.
+                Support game, with two different Motors inside, produce two different vibration effects.",
+                "image" => 'public\images\products\controller1.jpg'
+            ],
+            [
+                "title" => "Sports LED Wrist Watch Silicone Rubber Digital Running Womens Mens Boys Girls",
+                "price" => "2",
+                "category_id" => "1",
+                "description" => "The LED display time, month, day, seconds. Automatic calendar. Power saving mode.",
+                "image" => 'public\images\products\s-l1600.jpg'
+            ],
+            [
+                "title" => "USB-C Type C To 3.5mm Audio Aux Headphone Jack Cable Adapter",
+                "price" => "1.95",
+                "category_id" => "1",
+                "description" => "High fidelity sound quality. Built in high-performance DAC chip",
+                "image" => 'public\images\products\sa-l1600.jpg'
+            ],
+            [
+                "title" => "2V 24V 4.2A Bus Dual USB Charging Socket Power Adapter Car USB Charger",
+                "price" => "2.28",
+                "category_id" => "1",
+                "description" => "Universal Cigarette Lighter, USB Charger, Universal Mobile Phone",
+                "image" => 'public\images\products\g-l1600.jpg'
+            ],
+            [
+                "title" => "new women's clothing autumn slimming BC shiny handmade sequin linen silk knitwear hollow batwing sleeve shawl",
+                "price" => "40.11",
+                "category_id" => "4",
+                "description" => "BCShiny sequin cutout shawl. The mesh is soft and ideal for delicate and elegant modeling.
+                Exquisite material and mesh weaving combination.",
+                "image" => 'public\images\products\s-l1600d.jpg'
+            ],
+            [
+                "title" => "Run Vent Tech Grey/ Black Long Sleeve Shirt women's Sz Unknown sweater",
+                "price" => "22.49",
+                "category_id" => "4",
+                "description" => "Polyester/cotton",
+                "image" => 'public\images\products\as-l1600.jpg'
+            ],
+            [
+                "title" => "Summer Mens Slim Fit T Shir",
+                "price" => "3.29",
+                "category_id" => "3",
+                "description" => "Muscle Top Gym V Neck Short Sleeve Plain Tee Shirt",
+                "image" => 'public\images\products\ts-jhgf.jpg'
+            ],
+            [
+                "title" => "Elegant Mountain 925 Silver Filled Ring",
+                "price" => "1.59",
+                "category_id" => "3",
+                "description" => "Women/Men Jewelry Party Ring Sz 6-10",
+                "image" => 'public\images\products\s-lrng.jpg'
+            ]
+        ]);
     }
 }
