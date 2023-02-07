9e11957ab25bfa2dc65d62cc9eb77dc682c71af2
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-07 19:20:55 +0700

    fix: change styles

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -2,17 +2,24 @@
 @section('content')
     <div>
         @foreach($products as $product)
-            <div class="product">
-                <p>{{$product->title}}</p>
-
-                <div class="img_container">
-                    <img src="{{asset('../'.$product->image)}}" alt="{{asset($product->image)}}" width="300"/>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img class="img-fluid rounded-start" src="{{asset('../' . $product->image)}}"
+                             alt="{{asset($product->image)}}"
+                             width="300"/>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$product->title}}</h5>
+                            <h4 class="card-text py-3">{{$product->price}}$</h4>
+                            <p class="card-text">{{$product->description}}</p>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <a href="{{ route('admin.products.show', $product->id, $products) }}"><button type="button" class="btn btn-primary">Show</button></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <p>{{$product->price}} $</p>
-                <div>
-                    <p>{{$product->description}}</p>
-                </div>
-                <a href="{{ route('admin.products.show', $product->id, $products) }}">Show</a>
             </div>
         @endforeach
 
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,16 +1,32 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="product">
-        <p>{{$product->title}}</p>
-        <p>{{$product->price}}</p>
-        <img src="{{asset('../' . $product->image)}}" alt="{{asset($product->image)}}" width="300"/>
-        <p>{{$product->description}}</p>
-        <a href="{{ route('admin.products.edit', $product->id) }}">Edit</a>
-        <form action="{{ route('admin.products.delete', $product->id) }}" method="POST">
-            @csrf
-            @method('delete')
-            <input type="submit" value="Delete">
-        </form>
-        <a href="{{ route('admin.product.index') }}">Back</a>
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img class="img-fluid rounded-start" src="{{asset('../' . $product->image)}}"
+                     alt="{{asset($product->image)}}"
+                     width="300"/>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{$product->title}}</h5>
+                    <h4 class="card-text py-3">{{$product->price}}$</h4>
+                    <p class="card-text">{{$product->description}}</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <a href="{{ route('admin.products.edit', $product->id) }}">
+                            <button type="button" class="btn btn-primary">Edit</button>
+                        </a>
+                        <form action="{{ route('admin.products.delete', $product->id) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <input type="submit" value="Delete" class="btn btn-danger  mx-1">
+                        </form>
+                        <a href="{{ route('admin.product.index') }}">
+                            <button type="button" class="btn btn-secondary">Back</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
