05226bd9f198f1734caf72f7aff0920fd9a8adf8
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-09 17:14:58 +0700

    fix: add documentation page content. also add logo to home page

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,4 +1,56 @@
 @extends('layouts.main')
 @section('content')
-    <h1>Documentation</h1>
+    <div  class="m-lg-5">
+        <h1 class="">API Documentation</h1>
+
+        <pre>Note. If a product is added or updated, no changes are made to the database. The submitted object is returned.
+In case of deletion, the object with the specified id is returned.</pre>
+
+        <div class="grid gap-3 ">
+            <div class="m-2 p-2 g-col-6 border-bottom border-secondary">
+                <h2>Get all products</h2>
+                <p class="fs-5">Method: GET</p>
+                <code class="bg-light">http://fakestoreapi.ru/products</code>
+            </div>
+
+            <div class="m-2 p-2 g-6 border-bottom border-secondary">
+                <h3>Get a single product</h3>
+                <p>Method GET</p>
+                <p>Put the product number at the end of the line</p>
+                <code class="bg-light">https://fakestoreapi.ru/products/{id}</code>
+            </div>
+            <div class="m-2 p-2 g-col-6 border-bottom border-secondary">
+                <h3>Create new product</h3>
+                <p>Method POST</p>
+                <code class="bg-light">http://fakestoreapi.ru/products</code>
+                <p>Send an object</p>
+                <pre class="bg-light">[
+    'title' => 'required|string',
+    'price' => 'required|integer',
+    'category_id' => 'required|integer',
+    'description' => 'required|string',
+    'image' => 'string',
+]</pre>
+            </div>
+            <div class="m-2 p-2 g-col-6 border-bottom border-secondary">
+                <h3>Update a product</h3>
+                <p>Method PATCH</p>
+                <p>Send an object</p>
+                <pre class="bg-light">[
+    'title' => 'required|string',
+    'price' => 'required|integer',
+    'category_id' => 'required|integer',
+    'description' => 'required|string',
+    'image' => 'string',
+]</pre>
+            </div>
+            <div class="m-2 p-2 g-col-6 border-bottom border-secondary">
+                <h3>Delete a product</h3>
+                <p>Method DELETE</p>
+                <code class="bg-light">https://fakestoreapi.ru/products/{id}</code>
+                <p>Put the product number at the end of the line</p>
+            </div>
+
+        </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.main')
 @section('content')
     <div class="px-4 py-5 my-5 text-center">
-        <img class="d-block mx-auto mb-4" src="{{asset('images/logo.png')}}" alt="" width="50" height="50">
+        <img class="d-block mx-auto mb-4" src="{{asset('images/work.png')}}" alt="" width="100" height="100">
         <h1 class="display-5 fw-bold">Laravel Fake store API</h1>
         <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">Fake store rest API created using Laravel the php framework for training requests to the server</p>
+            <p class="lead mb-4">This site is not a real store. This is a way to practice sending requests to the server</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
                 <a href="https://github.com/Alex-Prokhorenko/laravel-mysql-store"><button type="button" class="btn btn-primary btn-lg px-4 gap-3">View on GitHub</button></a>
                 <a href="{{ route('show.docs') }}"><button type="button" class="btn btn-outline-secondary btn-lg px-4">Documentation</button></a>
